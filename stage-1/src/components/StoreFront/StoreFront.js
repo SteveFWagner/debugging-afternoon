7697141a4aa21ee7cb84bfbff3bd9e64f369007c
SteveFWagner <sfwagner1011@gmail.com>
2019-02-27 09:10:04 -0700

    assignment complete

--- stage-1/src/components/StoreFront/StoreFront.js
+++ stage-1/src/components/StoreFront/StoreFront.js
@@ -15,13 +15,14 @@ class StoreFront extends Component {
         axios.get("https://practiceapi.devmountain.com/products/")
             .then((response) => {
                 this.setState({
-                    products: response
+                    products: response.data
                 })
             })
     }
 
     render() {
-        let productDisplay = this.state.products.map((element, index) => {
+        let {products} = this.state
+        let productDisplay = products.map((element, index) => {
             return (
                 <div className="product-container" key={index}>
                     <h2>{element.title}</h2>
